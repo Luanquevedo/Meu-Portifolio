63acf57a8f188bf97e6c0afb26da7889053d6acd
Luanquevedo <151397956+Luanquevedo@users.noreply.github.com>
2024-01-24 10:09:28 -0300

    Update Projects.js

--- src/componentes/sections/Projects.js
+++ src/componentes/sections/Projects.js
@@ -11,23 +11,23 @@ function Projects(){
             <h1>Projetos</h1>
             <Cards
             img={bootcamp} 
-            title='Projeto bootcamp' 
+            title='Projeto de Bootcamp' 
             tech=' HTML, CSS' 
-            description='Uma landing page responsiva na qual possui o opjetivo de captação de dados para novos alunos.' 
+            description='Uma landing page responsiva com o objetivo de captar dados de novos alunos.' 
             rep='https://github.com/Luanquevedo/landingpage-2.git'
             site='https://stirring-seahorse-0de8d8.netlify.app'/>
             <Cards
             img={ccxp} 
             title='Projeto CCXP' 
             tech=' HTML, CSS, JavaScript'
-            description='Uma landing page na qual utiliza java script para exibir contador de tempo ate o inicio do evento proposto' 
+            description='Uma landing page que utiliza JavaScript para exibir um contador de tempo até o início do evento proposto.' 
             rep='https://github.com/Luanquevedo/ccxp-landingpage.git'
             site='https://projetoccxp2024.netlify.app'/>
             <Cards
             img={portifolio} 
-            title='Protifolio Pessoal' 
+            title='Portfólio Pessoal' 
             tech=' HTML, CSS, JavaScript, React, Bootstrap, React Icons'
-            description=' Um portifolio criado utilizando tecnologias mais refinadas com objetivo de apresentar o perfil e os projetos mais recentes realizados.' 
+            description=' Um portfólio criado utilizando tecnologias refinadas com o objetivo de apresentar meu perfil e os projetos mais recentes realizados.' 
             rep=''
             site=''/>
             <ButtonB text='Acesse o meu repositorio' href='https://github.com/Luanquevedo?tab=repositories'/>            
