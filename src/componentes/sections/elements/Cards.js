74269ab3b057c31b91daa14818fd52ccef35dc41
Luan Meira Quevedo <151397956+Luanquevedo@users.noreply.github.com>
2024-01-25 17:34:44 -0300

    Update Cards.js
    Realizada adição de renderização condicional nos cards apresentados.

--- src/componentes/sections/elements/Cards.js
+++ src/componentes/sections/elements/Cards.js
@@ -1,19 +1,33 @@
 import style from './Cards.module.css'
 import ButtonB from '../../sections/elements/ButtonB'
+import React, { useState } from 'react';
 
 function Cards({img, title, tech, description, rep, site} ){
-    return(
+
+    const [info, setInfo] = useState(false);
+
+    function handleMouseEnter() {
+        setInfo(true);
+    }
+
+    function handleMouseLeave() {
+        setInfo(false);
+    }
+
+    return (
         <div className={style.card}>
-            <a href={site}>
-                <img src={img} alt='Error'/>
+            <a onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} href={site}>
+                <img src={img} alt='Error' />
             </a>
-            
-            <section>
-                <h3> {title}</h3>
-                <p><strong>Tecnologia</strong>{tech}</p>
-                <p> {description}</p>
-                <ButtonB text='Acesse o repositorio' link={rep}/>
-            </section>
+
+            {info && (
+                <section>
+                    <h3>{title}</h3>
+                    <p><strong>Tecnologia:</strong> {tech}</p>
+                    <p>{description}</p>
+                    <ButtonB text='Acesse o repositorio' link={rep} />
+                </section>
+            )}
         </div>
     )
 }
